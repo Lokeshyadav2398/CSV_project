669ca79c5676c467f17d4d69b439c382065fb332
bhagavan <bhagavan@auranetworks.in>
2020-01-29 09:49:04 +0530

    Fixing buts

--- csv_data_sort.py
+++ csv_data_sort.py
@@ -1,31 +1,33 @@
 import csv 
+import pdb
 
 def open_csv_file(filename):
-	new_list = []
-	fd = open(filename)
-	fdata = csv.reader(fd)
-	
-	for row in fdata:
-		#print(row)
-		new_list.append(row)
-	
-	print("")
-	
-	#print(new_list)
-	temp = 0
-	for i in range(0,len(new_list)):
-		for j in range(i+1,len(new_list)):
-			if(new_list[i][0]>new_list[j][0]):
-				temp = new_list[i]
-				new_list[i] = new_list[j]
-				new_list[j] = temp
-	
-	print(new_list)	
-			
+    new_list = []
+    fd = open(filename)
+    fdata = csv.reader(fd)
+        
+    for row in fdata:
+        #print(row)
+        new_list.append(row)
+        
+    print("")
+        
+    temp = 0
+    pdb.set_trace()
+    for i in range(0,len(new_list)):
+        for j in range(i+1, len(new_list)):
+            if(int(new_list[i][0]) > int(new_list[j][0])):
+                temp = new_list[i]
+                new_list[i] = new_list[j]
+                new_list[j] = temp
+        
+    for line in new_list:
+        print(line)
+                        
 def main():
-	filename = "data.csv"
-	data = open_csv_file(filename)
-	return
-	
+    filename = "data.csv"
+    data = open_csv_file(filename)
+    return
+        
 if(__name__=="__main__"):
-	main()
+        main()
